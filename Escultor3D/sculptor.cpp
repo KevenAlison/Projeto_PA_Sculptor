d0a68d54e7f55932be27644e5df7b231293b5cc3
KevenAlison <86264992+KevenAlison@users.noreply.github.com>
2021-08-15 20:21:12 -0300

    Update sculptor.cpp

--- Escultor3D/sculptor.cpp
+++ Escultor3D/sculptor.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 //Construtor com alocação de memória
 Sculptor::Sculptor(int _nx, int _ny, int _nz){
-    int x,y;
+    int x,y,z;
     nx = _nx;
     ny = _ny;
     nz = _nz;
@@ -15,14 +15,19 @@ Sculptor::Sculptor(int _nx, int _ny, int _nz){
     v = new Voxel**[nx];
 
     for(x=0; x<nx; x++){
-         v[x] = new Voxel*[nx*ny];
-    }
-    for(x=0; x<nx; x++){
-        for(y=0; y<ny; y++){
-            v[x][y]= new Voxel[nz*ny*nx];
+         v[x] = new Voxel*[ny];
+         for(y=0; y<ny; y++){
+            v[x][y]= new Voxel[nz];
         }
     }
 
+    for(x=0; x<nx;x++){
+        for(y=0; y<nx;y++){
+            for(z=0; z<nx;z++){
+              v[x][y][z].isOn = false;
+            }
+        }
+    }
 
 }
 
@@ -70,116 +75,6 @@ void Sculptor::cutVoxel(int x, int y, int z){
     v[x][y][z].isOn = false;
 }
 
-//Ativa voxels entre um intervalo
-void Sculptor::putBox(int xi, int xf, int yi, int yf, int zi, int zf){
-    int cx, cy, cz;
-        for(cx=xi; cx<xf; cx++){
-          for(cy=yi; cy<yf; cy++){
-            for(cz=zi; cz<zf; cz++){
-
-                putVoxel(cx,cy,cz);
-
-            }
-           }
-         }
-
-}
-
-//Desativa voxels entre um intervalo
-void Sculptor::cutBox(int xi, int xf, int yi, int yf, int zi, int zf){
-    int cx, cy, cz; //coordenadas x,y,z
-    for(cx=xi; cx<=xf; cx++){
-        for(cy=yi; cy<=yf; cy++){
-            for(cz=zi; cz<=zf; cz++){
-
-                cutVoxel(cx,cy,cz);
-
-            }
-        }
-    }
-
-}
-
-//Habilitar voxels dentro dos limites da esfera
-void Sculptor::putSphere(int xc, int yc, int zc, int raio){
-    int x,y,z,dxs,dys,dzs;
-    if( ((xc+raio)>=nx || (xc+raio)<0)  ||  ((yc+raio)>=ny || (yc+raio)<0)  ||  ((zc+raio)>=nz || (zc+raio)<0)){
-        cout << "Valores indicados ultrapassam os limites da matriz " << nx << " x " << ny << " x " << nz << endl;
-        exit(0);
-    }
-    for(x=0; x<=nx; x++){
-        for(y=0; y<=ny; y++){
-            for(z=0; z<=nz; z++){
-                dxs=x-xc; dys=y-yc;dzs=z-zc;
-                if((pow(dxs,2)+pow(dys,2)+pow(dzs,2))<=pow(raio,2)){
-                    putVoxel(x,y,z);
-                }
-            }
-        }
-    }
-
-}
-
-
-//Desabilitar voxels dentro dos limites da esfera
-void Sculptor::cutSphere(int xc, int yc, int zc, int raio){
-    if( ((xc+raio)>=nx || (xc+raio)<0)  ||  ((yc+raio)>=ny || (yc+raio)<0)  ||  ((zc+raio)>=nz || (zc+raio)<0)){
-        cout << "Valores indicados ultrapassam os limites da matriz " << nx << " x " << ny << " x " << nz << endl;
-        exit(0);
-    }
-    int x,y,z,dxs,dys,dzs;
-    for(x=0; x<=nx; x++){
-        for(y=0; y<=ny; y++){
-            for(z=0; z<=nz; z++){
-                dxs=x-xc; dys=y-yc;dzs=z-zc;
-                if((pow(dxs,2)+pow(dys,2)+pow(dzs,2))<=pow(raio,2)){
-                    cutVoxel(x,y,z);
-                }
-            }
-        }
-    }
-
-}
-
-//Habilitar voxels dentro dos limites da elipsoide
-void Sculptor::putEllipsoid(int xc, int yc, int zc, int rx, int ry, int rz){
-    if( ((xc+rx)>=nx || (xc+rx)<0)  ||  ((yc+ry)>=ny || (yc+ry)<0)  ||  ((zc+rz)>=nz || (zc+rz)<0)){
-        cout << "Valores indicados ultrapassam os limites da matriz " << nx << " x " << ny << " x " << nz << endl;
-        exit(0);
-    }
-    int x,y,z;
-    for(x=0; x<=nx; x++){
-        for(y=0; y<=ny; y++){
-            for(z=0; z<=nz; z++){
-                //Equação da elipse
-                if((pow((x-xc),2) / pow(rx,2) + pow((y-yc),2) / pow(ry,2) + pow((z-zc),2) / pow(rz,2)) <= 1.0){
-                    putVoxel(x,y,z);
-                }
-            }
-        }
-    }
-
-}
-
-//Desabilitar voxels dentro dos limites da elipsoide
-void Sculptor::cutEllipsoid(int xc, int yc, int zc, int rx, int ry, int rz){
-    if( ((xc+rx)>=nx || (xc+rx)<0)  ||  ((yc+ry)>=ny || (yc+ry)<0)  ||  ((zc+rz)>=nz || (zc+rz)<0)){
-        cout << "Valores indicados ultrapassam os limites da matriz " << nx << " x " << ny << " x " << nz << endl;
-        exit(0);
-    }
-    int x,y,z;
-    for(x=0; x<=nx; x++){
-        for(y=0; y<=ny; y++){
-            for(z=0; z<=nz; z++){
-                //Equação da elipse
-                if((pow((x-xc),2) / pow(rx,2) + pow((y-yc),2) / pow(ry,2) + pow((z-zc),2) / pow(rz,2)) <= 1.0){
-                    cutVoxel(x,y,z);
-                }
-            }
-        }
-    }
-
-}
 
 //Gerar aquivo .OFF
 void Sculptor::writeOFF(const char *nome){
